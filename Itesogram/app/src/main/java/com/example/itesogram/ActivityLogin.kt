9ad35c381a8b9689c46e1b1ae3b00417560467ba
DiegoAscencio <is698355@iteso.mx>
2019-09-11 07:25:30 -0500

    Parse view finished

--- Itesogram/app/src/main/java/com/example/itesogram/ActivityLogin.kt
+++ Itesogram/app/src/main/java/com/example/itesogram/ActivityLogin.kt
@@ -9,6 +9,7 @@ class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
     }
 
     fun mRegister(view: View) {
